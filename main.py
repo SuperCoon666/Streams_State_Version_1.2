5de94ea472ed78f6771e914f328bc10fae15ded5
SuperCoon <56171741+SuperCoon666@users.noreply.github.com>
2021-10-05 15:06:03 +0300

    Enter numbers of false links

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ def query_date(u):
 
 def link_check(start):
     try:
-        if not start.strip():
-            return
-
         source = start.replace("\n", '')
         ind = source.index("'")
         link = source[ind:].replace("'",'')
@@ -29,9 +26,19 @@ def link_check(start):
     except:
         return "Error: "+ str(sys.exc_info()[1])
 
-f = open('C:\\Users\\Vlad\\Desktop\\Data_Streams.txt')
+f = open('C:\\Users\\Vlad\\Desktop\\Data_Streams(1).txt')
 f = f.readlines()
+numbers = ""
 for i in range(len(f)):
+    if not f[i].strip():
+            continue
     result = link_check(f[i])
-    if result != None:
-        print(result)
+
+    if "False" in result:
+        ind = f[i].index("'")
+        numb_i = f[i]
+        numb_i = numb_i[:ind]
+        numbers+= " "+numb_i
+
+    print(result)
+print(numbers)
